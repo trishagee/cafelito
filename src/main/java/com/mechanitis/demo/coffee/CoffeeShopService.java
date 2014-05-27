1a3940771ef7b41a97f705fa982352f8d069cb2d
Trisha Gee <trishagee@github.com>
2014-05-27 16:14:29 +0200

    [Story 0] As a user of this amazing application, I need to be able to see it

---
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopService.java
@@ -0,0 +1,23 @@
+package com.mechanitis.demo.coffee;
+
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.assets.AssetsBundle;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+
+public class CoffeeShopService extends Service<CoffeeShopConfiguration> {
+    public static void main(final String[] args) throws Exception {
+        new CoffeeShopService().run(args);
+    }
+    
+    @Override
+    public void initialize(final Bootstrap bootstrap) {
+        AssetsBundle bundle = new AssetsBundle("/html", "/");
+        bootstrap.addBundle(bundle);
+    }
+
+    @Override
+    public void run(final CoffeeShopConfiguration configuration, final Environment environment) throws Exception {
+        environment.addResource(new CoffeeShopResource());
+    }
+}
