11e66999933b907f0ef0cab0828375b6a9995c4b
Trisha Gee <trishagee@github.com>
2014-07-24 14:58:15 +0200

    [Story 0] "As a user of this amazing application, I need to be able to see it" - Drop wizard back end up and running.

---
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
@@ -0,0 +1,10 @@
+package com.mechanitis.demo.coffee;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/coffeeshop")
+@Produces(MediaType.APPLICATION_JSON)
+public class CoffeeShopResource {
+}
