f329252387bf14775a88847156dd2b7b7fbcf82c
Trisha Gee <trishagee@github.com>
2014-09-19 17:07:30 +0200

    [Story 6] "As a normal paranoid human being, I want to see my order made it to the server" - Added success alerts to the front end using UI Bootstrap

--- src/main/resources/html/js/coffee.js
+++ src/main/resources/html/js/coffee.js
@@ -1,4 +1,4 @@
-var coffeeApp = angular.module('coffeeApp', ['ngResource']);
+var coffeeApp = angular.module('coffeeApp', ['ngResource', 'ui.bootstrap']);
 
 coffeeApp.factory('CoffeeOrder', function ($resource) {
     return $resource('/service/coffeeshop/:id/order/',
@@ -14,8 +14,17 @@ coffeeApp.controller('OrderController', function ($scope, CoffeeOrder) {
         {name: 'Cappuccino', family: 'Coffee'}
     ];
     $scope.sizes = ['Small', 'Medium', 'Large'];
+    $scope.messages = [];
 
     $scope.giveMeCoffee = function () {
-        CoffeeOrder.save({id:1}, $scope.drink)
-    }
+        CoffeeOrder.save({id:1}, $scope.drink, function (order) {
+                $scope.messages.push({type: 'success', msg: 'Order sent!'})
+            }
+        )
+    };
+
+    $scope.closeAlert = function (index) {
+        $scope.messages.splice(index, 1);
+    };
+
 });
