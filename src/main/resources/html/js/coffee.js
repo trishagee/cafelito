46f6500dbe4c97a5ac88cf2a4b02b7f69f2c34f6
Trisha Gee <trishagee@github.com>
2014-09-25 17:49:38 -0700

    [Lesson 7] Taking Advantage of MongoDB’s Dynamic Schema

--- src/main/resources/html/js/coffee.js
+++ src/main/resources/html/js/coffee.js
@@ -8,6 +8,26 @@ coffeeApp.controller('OrderController', function ($scope, $resource) {
         {name: 'Cappuccino', family: 'Coffee'}
     ];
     $scope.sizes = ['Small', 'Medium', 'Large'];
+    $scope.availableOptions = [
+        {name: 'soy milk', appliesTo: 'milk'} ,
+        {name: 'skimmed', appliesTo: 'milk'},
+        {name: 'caramel', appliesTo: 'syrup'},
+        {name: 'decaf', appliesTo: 'caffeine'},
+        {name: 'whipped Cream', appliesTo: 'extras'},
+        {name: 'vanilla', appliesTo: 'syrup'},
+        {name: 'hazelnut', appliesTo: 'syrup'},
+        {name: 'sugar free', appliesTo: 'syrup'},
+        {name: 'non fat', appliesTo: 'milk'},
+        {name: 'half fat', appliesTo: 'milk'},
+        {name: 'half and half', appliesTo: 'milk'},
+        {name: 'half caf', appliesTo: 'caffeine'},
+        {name: 'chocolate powder', appliesTo: 'extras'},
+        {name: 'double shot', appliesTo: 'preparation'},
+        {name: 'wet', appliesTo: 'preparation'},
+        {name: 'dry', appliesTo: 'preparation'},
+        {name: 'organic', appliesTo: 'milk'},
+        {name: 'extra hot', appliesTo: 'preparation'}
+    ];
 
     $scope.messages = [];
 
@@ -23,5 +43,14 @@ coffeeApp.controller('OrderController', function ($scope, $resource) {
     $scope.closeAlert = function (index) {
         $scope.messages.splice(index, 1);
     };
+
+    $scope.addOption = function () {
+        if (!$scope.drink.selectedOptions) {
+            $scope.drink.selectedOptions = [];
+        }
+        $scope.drink.selectedOptions.push($scope.newOption);
+        $scope.newOption = '';
+    };
+
 
 });
