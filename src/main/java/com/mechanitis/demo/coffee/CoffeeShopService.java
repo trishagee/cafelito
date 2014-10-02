25c6d77eaf861b051808044a5dbdfb244fa002f9
Trisha Gee <trishagee@github.com>
2014-10-02 14:59:22 -0700

    [Story 0] "As a user of this amazing application, I need to be able to see it" - Drop wizard back end up and running.

---
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopService.java
@@ -0,0 +1,24 @@
+package com.mechanitis.demo.coffee;
+
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.assets.AssetsBundle;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Configuration;
+import com.yammer.dropwizard.config.Environment;
+
+public class CoffeeShopService extends Service<Configuration> {
+    public static void main(String[] args) throws Exception {
+        new CoffeeShopService().run(args);
+    }
+    
+    @Override
+    public void initialize(final Bootstrap<Configuration> bootstrap) {
+        AssetsBundle bundle = new AssetsBundle("/html", "/");
+        bootstrap.addBundle(bundle);
+    }
+
+    @Override
+    public void run(final Configuration configuration, final Environment environment) throws Exception {
+        environment.addResource(new CoffeeShopResource());
+    }
+}
