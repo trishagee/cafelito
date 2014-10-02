1b7045ffb438c8223eb5b44d0e706ae3ac58006e
Trisha Gee <trishagee@github.com>
2014-10-02 15:35:41 -0700

    [Story 6] "As a thirsty person, I want to know where to pick my coffee up from" - Server-side code to find the nearest coffee shop to a given location

--- src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
@@ -1,13 +1,19 @@
 package com.mechanitis.demo.coffee;
 
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.QueryBuilder;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
@@ -19,6 +25,21 @@ public class CoffeeShopResource {
 
     public CoffeeShopResource(final MongoClient mongoClient) {
         datastore = new Morphia().createDatastore(mongoClient, "Cafelito");
+    }
+    
+    @Path("nearest/{latitude}/{longitude}")
+    @GET
+    public Object getNearest(@PathParam("latitude") double latitude, @PathParam("longitude") double 
+    longitude) {
+        DBCollection collection = datastore.getDB().getCollection("CoffeeShop");
+
+        DBObject coffeeShop = collection.findOne(QueryBuilder.start("location").nearSphere(longitude, latitude).get());
+
+        if (coffeeShop == null) {
+            throw new WebApplicationException(404);
+        }
+
+        return coffeeShop;
     }
 
     @Path("order")
