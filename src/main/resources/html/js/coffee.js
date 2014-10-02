f1bee2b6fa89e5c5d6264161c1642cb7cc221ce9
Trisha Gee <trishagee@github.com>
2014-10-02 15:07:45 -0700

    [Story 1] "As a coffee drinker, I would like to be able to select the coffee I want to order" - HTML page with AngularJS drop down populated with drink types.

---
+++ src/main/resources/html/js/coffee.js
@@ -0,0 +1,12 @@
+var coffeeApp = angular.module('coffeeApp', []);
+
+coffeeApp.controller('OrderController', function ($scope) {
+    $scope.types = [
+        {name: 'Americano', family: 'Coffee'},
+        {name: 'Latte', family: 'Coffee'},
+        {name: 'Tea', family: 'that other drink'},
+        {name: 'Cappuccino', family: 'Coffee'}
+    ];
+    $scope.sizes = ['Small', 'Medium', 'Large'];
+
+});
