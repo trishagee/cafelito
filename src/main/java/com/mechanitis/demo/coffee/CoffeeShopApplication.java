8d817aa75a7c4b124722f3006c0eac7f00cea9a4
Trisha Gee <trishagee@users.noreply.github.com>
2015-02-03 14:00:07 +0100

    Story 0

---
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopApplication.java
@@ -0,0 +1,24 @@
+package com.mechanitis.demo.coffee;
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class CoffeeShopApplication extends Application<Configuration> {
+    public static void main(String[] args) throws Exception {
+        new CoffeeShopApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<Configuration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/html", "/coffeeshop", "coffee.html"));
+    }
+
+    @Override
+    public void run(Configuration configuration, Environment environment) throws Exception {
+        environment.jersey().setUrlPattern("/service");
+        environment.jersey().register(new CoffeeShopResource());
+    }
+}
