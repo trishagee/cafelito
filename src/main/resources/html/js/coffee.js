f0ad3bb3740ee30479d6c346e984eb79937e3ae3
Trisha Gee <trishagee@users.noreply.github.com>
2015-02-03 15:19:52 +0100

    Story 1

---
+++ src/main/resources/html/js/coffee.js
@@ -0,0 +1,12 @@
+var coffeeApp = angular.module('coffeeApp', []);
+
+coffeeApp.controller('OrderController', function ($scope) {
+    $scope.types = [
+        {name: 'Cappuccino', family: 'coffee'},
+        {name: 'Latte', family: 'coffee'},
+        {name: 'Tea', family: 'that other drink'},
+        {name: 'Espresso', family: 'coffee'}
+    ];
+    $scope.sizes = ['Small', 'Medium', 'Large'];
+
+});
