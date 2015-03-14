a9af8a9e4472522596959eaa165366aed4134e3b
Trisha Gee <trisha.gee@gmail.com>
2015-03-14 14:17:45 +0100

    [Story 5] "As a thirsty person, I want to know where to pick my coffee up from" - Server-side code to find the nearest coffee shop to a given location, plus client-side code to call it (NEEDS CHECKING)

--- src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
@@ -5,8 +5,10 @@ import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,6 +18,7 @@ import java.net.URI;
 import static java.net.URI.create;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.Response.created;
+import static org.mongodb.morphia.geo.PointBuilder.pointBuilder;
 
 @Path("/coffeeshop")
 @Produces(APPLICATION_JSON)
@@ -24,6 +27,17 @@ public class CoffeeShopResource {
 
     public CoffeeShopResource(MongoClient mongoClient) {
         datastore = new Morphia().createDatastore(mongoClient, "Cafelito");
+    }
+
+    @Path("nearest/{latitude}/{longitude}")
+    @GET
+    public Object getNearest(@PathParam("latitude") double latitude,
+                             @PathParam("longitude") double longitude) {
+        return datastore.find(CoffeeShop.class)
+                        .field("location")
+                        .near(pointBuilder().latitude(latitude)
+                                            .longitude(longitude).build(), 2000)
+                        .get();
     }
 
     @Path("order")
--- src/main/resources/html/js/coffee.js
+++ src/main/resources/html/js/coffee.js
@@ -1,6 +1,35 @@
 var coffeeApp = angular.module('coffeeApp', ['ngResource', 'ui.bootstrap']);
 
-coffeeApp.controller('OrderController', function ($scope, $resource) {
+coffeeApp.service('LocalCoffeeShop', function () {
+    var localCoffeeShop;
+
+    this.setShop = function (shop) {
+        localCoffeeShop = shop;
+    };
+
+    this.getShop = function () {
+        return localCoffeeShop;
+    }
+});
+
+coffeeApp.controller('CoffeeShopController', function ($scope, $window, $resource, LocalCoffeeShop) {
+    var CoffeeShopLocator = $resource('/service/coffeeshop/nearest/:latitude/:longitude',
+        {latitude: '@latitude', longitude: '@longitude'}, {});
+
+    $scope.findCoffeeShopNearestToMe = function () {
+        window.navigator.geolocation.getCurrentPosition(function (position) {
+            CoffeeShopLocator.get({latitude: position.coords.latitude, longitude: position.coords.longitude},
+                function (foundCoffeeShop) {
+                    $scope.nearestCoffeeShop = foundCoffeeShop;
+                    LocalCoffeeShop.setShop(foundCoffeeShop);
+                });
+        });
+    };
+    $scope.findCoffeeShopNearestToMe();
+});
+
+
+coffeeApp.controller('OrderController', function ($scope, $resource, LocalCoffeeShop) {
     $scope.types = [
         {name: 'Cappuccino', family: 'coffee'},
         {name: 'Latte', family: 'coffee'},
@@ -11,7 +40,7 @@ coffeeApp.controller('OrderController', function ($scope, $resource) {
     $scope.messages = [];
 
     $scope.giveMeCoffee = function() {
-        $scope.drink.coffeeShopId = 1;
+        $scope.drink.coffeeShopId = LocalCoffeeShop.getShop().coffeeShopId;
         var CoffeeOrder = $resource('/service/coffeeshop/order/');
         CoffeeOrder.save($scope.drink,
             function (order) {
