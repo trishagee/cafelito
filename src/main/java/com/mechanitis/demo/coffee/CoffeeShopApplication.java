3ea22817db004c0d9f984907afc89b32c0515daa
Trisha Gee <trisha.gee@gmail.com>
2015-03-14 08:39:26 +0100

    [Story 0] "As a user of this amazing application, I need to be able to see it" - Drop wizard back end up and running.

---
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopApplication.java
@@ -0,0 +1,25 @@
+package com.mechanitis.demo.coffee;
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class CoffeeShopApplication extends Application<Configuration> {
+    public static void main(String[] args) throws Exception {
+        new CoffeeShopApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/html/", "/coffeeshop/"));
+    }
+
+    @Override
+    public void run(Configuration configuration, Environment environment)
+            throws Exception {
+        environment.jersey().register(new CoffeeShopResource());
+        environment.jersey().setUrlPattern("/service/*");
+    }
+}
---
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
@@ -0,0 +1,11 @@
+package com.mechanitis.demo.coffee;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+@Path("/coffeeshop")
+@Produces(APPLICATION_JSON)
+public class CoffeeShopResource {
+}
