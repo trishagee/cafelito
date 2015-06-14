ade8966e5ad7f983639bcac0127d930be31ec0b7
Trisha Gee <trisha.gee@gmail.com>
2015-06-14 22:54:59 +0200

    [Story 6] "As a thirsty person, I want to know where to pick my coffee up from" - Server-side code to find the nearest coffee shop to a given location, plus client-side code to call it

--- src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
+++ src/main/java/com/mechanitis/demo/coffee/CoffeeShopResource.java
@@ -5,8 +5,10 @@ import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -14,6 +16,7 @@ import javax.ws.rs.core.Response;
 import java.net.URI;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.mongodb.morphia.geo.PointBuilder.pointBuilder;
 
 @Path("/coffeeshop")
 @Produces(APPLICATION_JSON)
@@ -23,6 +26,18 @@ public class CoffeeShopResource {
     public CoffeeShopResource(MongoClient mongoClient) {
         datastore = new Morphia().createDatastore(mongoClient, "Cafelito");
     }
+
+    @Path("nearest/{latitude}/{longitude}")
+    @GET
+    public Object getNearest(@PathParam("latitude") double latitude,
+                             @PathParam("longitude") double longitude) {
+        return datastore.find(CoffeeShop.class)
+                        .field("location")
+                        .near(pointBuilder().latitude(latitude)
+                                            .longitude(longitude).build(), 2000)
+                        .get();
+    }
+
 
     @Path("order")
     @POST()
